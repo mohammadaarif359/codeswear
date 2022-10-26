4dcfcb2a307a7a8c4dc7082a53b6a15aff2b3510
aarif <aarif@technoscore.net>
2022-10-26 13:27:25 +0530

    changes

--- pages/admin/allproducts.js
+++ pages/admin/allproducts.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../src/theme/theme";
 import FullLayout from "../../src/layouts/FullLayout";
-import { Grid } from "@mui/material";
+import { Grid, Pagination,PaginationItem, Stack } from "@mui/material";
 import ProductsTable from "../../src/components/dashboard/ProductsTable";
 const mongoose = require('mongoose');
 import Product from '../../models/Product'
 
-const AllProducts = ({products,logout}) => {
-  console.log('products',products)	
+const AllProducts = ({products,pages,logout}) => {
+  const router = useRouter();
+  const [page,setPage] = useState(router.query.page ? router.query.page : 1)
+  const handleChange = (event,value) =>{
+	  setPage(value);
+	  router.push('/admin/allproducts?page='+value)
+  }  
   return (
     <ThemeProvider theme={theme}>
 	  <FullLayout logout={logout}>
 		<Grid container spacing={0}>
 		  <Grid item xs={12} lg={12}>
 			<ProductsTable products={products}/>
+			<Pagination count={parseInt(pages)} page={parseInt(page)} onChange={handleChange} color="primary" />
 		  </Grid>
 		</Grid>
 	  </FullLayout>
@@ -24,14 +31,18 @@ const AllProducts = ({products,logout}) => {
 
 export default AllProducts
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context,page) {
   if(!mongoose.connections[0].readyState) {
     const con = await mongoose.connect(process.env.MONGO_URI)
   }
   //console.log(`MongoDB Connected : ${con.connection.host}`)
-  let products = await Product.find();
+  let pageSize = 5;
+  
+  let products = await Product.find({}).skip((page - 1) * pageSize).limit(pageSize);
+  let count = await Product.count()
+  let pages = Math.ceil(count/pageSize);
   
   return {
-    props: {products:JSON.parse(JSON.stringify(products))}, // will be passed to the page component as props
+    props: {products:JSON.parse(JSON.stringify(products)),pages:pages}, // will be passed to the page component as props
   }
 }
