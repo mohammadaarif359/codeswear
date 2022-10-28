31a9263723f5265b772412950cd6fe2886402ce2
aarif <aarif@technoscore.net>
2022-10-28 16:05:11 +0530

    express validation in update product api

--- pages/admin/editproduct.js
+++ pages/admin/editproduct.js
@@ -23,6 +23,7 @@ const EditProduct = () => {
   console.log('id',router.query.id)
   const [productId,setProductId] = useState('')
   const [form,setForm] = useState({})
+  const [error,setError] = useState({})
   
   // handleChange
   const handleChange =(e)=>{
@@ -64,12 +65,21 @@ const EditProduct = () => {
 	let res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/updateproducts`,{
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify([form])
+		body: JSON.stringify(form)
     });  
     res = await res.json();
 	if(res.success) {
 		router.push('/admin/allproducts')
 		alert(res.success);
+	} else if(res.errors) {
+		let errors = res.errors;
+		console.log('errors',errors)
+		{errors.forEach(val => {
+			setError((error)=>{
+				return {...error,[val.param]:val.msg}
+			})
+		});}	
+		console.log('form error',error)
 	} else {
 		alert(res.error);
 	}
@@ -81,19 +91,20 @@ const EditProduct = () => {
 		  <Grid item xs={12} lg={12}>
 			<BaseCard title="Edit Product">
 			  <Stack spacing={3}>
-			    <TextField type='hidden' onChange={handleChange} value={form._id ? form._id : ''} name="_id" label="" variant="outlined" />
-				<TextField onChange={handleChange} value={form.title ? form.title : ''} name="title" label="Title" variant="outlined" />
-				<TextField onChange={handleChange} value={form.slug ? form.slug : ''} name="slug" label="Slug" variant="outlined" />
-				<TextField onChange={handleChange} value={form.category ? form.category : ''} name="category" label="Category" variant="outlined" />
-				<TextField onChange={handleChange} value={form.price ? form.price : ''} name="price" label="Price" variant="outlined" />
-				<TextField onChange={handleChange} value={form.availableQty ? form.availableQty : ''} name="availableQty" label="Available Qty" variant="outlined" />
-				<TextField onChange={handleChange} value={form.size ? form.size : ''} name="size" label="Size" variant="outlined" />
-				<TextField onChange={handleChange} value={form.color ? form.color : ''} name="color" label="Color" variant="outlined" />
+			    <TextField type='hidden' onChange={handleChange} value={form._id ? form._id : ''} name="_id" label="" variant="outlined" helperText={error._id ? error._id : ''}/>
+				<TextField onChange={handleChange} value={form.title ? form.title : ''} name="title" label="Title" variant="outlined" helperText={error.title ? error.title : ''}/>
+				<TextField onChange={handleChange} value={form.slug ? form.slug : ''} name="slug" label="Slug" variant="outlined" helperText={error.slug  ? error.slug : ''}/>
+				<TextField onChange={handleChange} value={form.category ? form.category : ''} name="category" label="Category" variant="outlined" helperText={error.category ? error.category : ''}/>
+				<TextField onChange={handleChange} value={form.price ? form.price : ''} name="price" label="Price" variant="outlined" helperText={error.price ? error.price : ''}/>
+				<TextField onChange={handleChange} value={form.availableQty ? form.availableQty : ''} name="availableQty" label="Available Qty" variant="outlined" helperText={error.availableQty ? error.availableQty : ''}/>
+				<TextField onChange={handleChange} value={form.size ? form.size : ''} name="size" label="Size" variant="outlined" helperText={error.size ? error.size : ''}/>
+				<TextField onChange={handleChange} value={form.color ? form.color : ''} name="color" label="Color" variant="outlined" helperText={error.color ? error.color : ''}/>
 				<TextField onChange={handleChange}
 				  value={form.desc ? form.desc : ''} name="desc"
 				  label="Description"
 				  multiline
 				  rows={4}
+				  helperText={error.desc ? error.desc : ''} className="error_desc"
 				/>
 			  </Stack>
 			  <br />
--- pages/api/updateproducts.js
+++ pages/api/updateproducts.js
@@ -1,24 +1,36 @@
 import Product from '../../models/Product'
 import connectDb from '../../middleware/mongoose'
+import initMiddleware from '../../middleware/init-middleware'
+import validateMiddleware from '../../middleware/validate-middleware'
+import { check, validationResult } from 'express-validator'
+
+const validateBody = initMiddleware(
+    validateMiddleware([
+        check('title','Title is required').notEmpty(),
+		check('slug','Slug is required').notEmpty().custom((value,{req}) => {
+          return Product.findOne({ slug: value,_id:{ $ne: req.body._id } }).then((product) => {
+            if (product) {
+              return Promise.reject('Slug already exits');
+            }
+          });
+        }),
+		check('category','Catgory is required').notEmpty(),
+		check('price','Price between 1 to 10000').notEmpty().isInt({ min: 1, max: 10000}),
+		check('availableQty','AvailableQty between 1 to 100').isInt({ min: 1, max: 100}),
+		check('size','Size is required').notEmpty(),
+		check('color','Color is required').notEmpty(),
+		check('desc','Desc is required').notEmpty(),
+    ], validationResult)
+)
 
 const handler = async(req,res) =>{
     if(req.method == 'POST') {
-		console.log('req body update',req.body);
-        for(let i=0;i<req.body.length;i++) {
+		// check validation
+		await validateBody(req, res)
+		/*for(let i=0;i<req.body.length;i++) {
             let p = await Product.findByIdAndUpdate(req.body[i]._id,req.body[i])
-            /*let p = new Product({
-                title:req.body[i].title,
-                slug:req.body[i].slug,
-                desc:req.body[i].desc,
-                img:req.body[i].img,
-                category:req.body[i].category,
-                size:req.body[i].size,
-                color:req.body[i].color,
-                price:req.body[i].price,
-                availableQty:req.body[i].availableQty    
-            })
-            await p.save();*/
-        }
+        }*/
+		let p = await Product.findByIdAndUpdate(req.body._id,req.body)
         res.status(200).json({success:"Product updated successfully"});
     } else {
         res.status(200).json({error:"Method not allowed"});
