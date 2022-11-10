640a42fb338fcb997da9c7ed855fff59c1e43aa2
aarif <aarif@technoscore.net>
2022-11-10 13:19:43 +0530

    security headers added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=0, must-revalidate, no-store, nocache, private',
+          },
+          {
+			key: 'X-Content-Type-Options',
+			value: 'nosniff',
+		  },
+		  {
+			key: 'X-Frame-Options',
+			value: 'DENY',
+		  },
+		  {
+			key: 'X-XSS-Protection',
+			value: '1; mode=block',
+		  },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
